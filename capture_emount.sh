afd6f2e9fb861ac3f7fe6657ca72aafa60ad16fe
Andy Dodd <atd7@cornell.edu>
2017-06-13 18:52:12 -0400

    Update channel IDs for new sigrok configuration

--- capture_emount.sh
+++ capture_emount.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sigrok-cli -d fx2lafw -c samplerate=6M --continuous -C 0=BODY_VD_LENS,1=VCC,2=LENS_CS_BODY,3=RXD,4=TXD,5=BODY_CS_LENS -t VCC=1 -w -O srzip -o $1
+sigrok-cli -d fx2lafw -c samplerate=6M --continuous -C D0=BODY_VD_LENS,D1=VCC,D2=LENS_CS_BODY,D3=RXD,D4=TXD,D5=BODY_CS_LENS -t VCC=1 -w -O srzip -o $1
